c03bf96bf70531dc3cfd908393d0167a082e8187
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2025-08-26 18:10:47 +0530

    Added Internal Docs from EmailGeneratorControlller

--- src/main/java/org/email/writer/app/EmailGeneratorControlller.java
+++ src/main/java/org/email/writer/app/EmailGeneratorControlller.java
@@ -1,20 +1,44 @@
 package org.email.writer.app;
 
-import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor; // To reduce boilerplate code by injecting getter and setter at runtime
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+/* It serves data(like JSON) while controller serves entire web page where
+ we get redirected,but it just gives the list or data
+ */
+
 @RequestMapping("api/email/")
-@AllArgsConstructor
+/* API Endpoints are specific urls where you can send and receive data
+    and once the user hit the url in braces it starts generateEmail method
+ */
+@AllArgsConstructor // for generating all argument constructor for class
+
 @CrossOrigin(origins = "*")
+/*
+    Cross Origin Resources Sharing
+    when you see a X or YT video embedded in news article they both are on different domain but still we see on same web and domain. Usually browser doesn't allow but this let
+    us do
+ */
 public class EmailGeneratorControlller {
     private final EmailGeneratorService emailGeneratorService;
 
     @PostMapping("generate")
+
+    /**
+     * ResponseEntity gives full access to HTTP response it doesn't only @return actual data but also status code and header(metaData of information)
+        * if it was returning String it was always 200 OK but when it's issue from users end it will respond with badRequest or other ones.
+     * if it is null or empty it throws bad request otherwise it call generateEmailReply and stores in response and
+     * send back the  response to the user
+     */
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) throws ResponseStatusException {
+        /*
+        RequestBody accepts request from user
+         */
+
         if (emailRequest == null || emailRequest.getEmailContent() == null || emailRequest.getEmailContent().isBlank()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "emailContent must not be empty");
         }
