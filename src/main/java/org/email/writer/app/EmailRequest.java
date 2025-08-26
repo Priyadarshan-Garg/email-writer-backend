5cce39ea4e048cd1911da12501ce1c2e7ec72ee6
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2025-08-26 18:14:27 +0530

    Added Internal Docs for EmailRequest

--- src/main/java/org/email/writer/app/EmailRequest.java
+++ src/main/java/org/email/writer/app/EmailRequest.java
@@ -1,9 +1,14 @@
 package org.email.writer.app;
 
 import lombok.Data;
+/*
+    Pojo class for modifying the data
+ */
+@Data // Lombok annotation for generating setter getters toString and many more...
 
-@Data
 public class EmailRequest {
+    // Ensures the content of email if it's empty then it will show user BAD request
     private String emailContent;
+    // to convert the tone as per user's choice
     private String tone;
 }
